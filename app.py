00b32b298705e384328c06d5bb50e08eb40df9d1
JosuaLimbu <osaltusal@gmail.com>
2024-02-19 10:24:48 +0800

    test button start cam

--- app.py
+++ app.py
@@ -16,39 +16,9 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route("/detect", methods=['POST'])
-def detect():
-    if not request.method == "POST":
-        return
-    video = request.files['video']
-    video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
-    print(video)
-    subprocess.run("ls")
-    subprocess.run(['python3', 'detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename))])
-
-    # return os.path.join(uploads_dir, secure_filename(video.filename))
-    obj = secure_filename(video.filename)
-    return obj
-
 @app.route("/opencam", methods=['GET'])
 def opencam():
     print("here")
     subprocess.run(['python3', 'detect.py', '--source', '0'])
-    return "done"
+    return render_template('index.html')
     
-
-@app.route('/return-files', methods=['GET'])
-def return_file():
-    obj = request.args.get('obj')
-    loc = os.path.join("runs/detect", obj)
-    print(loc)
-    try:
-        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
-        # return send_from_directory(loc, obj)
-    except Exception as e:
-        return str(e)
-
-# @app.route('/display/<filename>')
-# def display_video(filename):
-# 	#print('display_video filename: ' + filename)
-# 	return redirect(url_for('static/video_1.mp4', code=200))
