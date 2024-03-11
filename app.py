91a71d292f8f1eaed9235fd5e1b06371b845a9d3
JosuaLimbu <osaltusal@gmail.com>
2024-03-11 21:28:08 +0800

    videostream in web

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import subprocess
 import os
 
 app = Flask(__name__)
 
+IMAGE_FOLDER = "videostream"
 # Variabel global untuk menyimpan proses yang sedang berjalan
 running_process = None
 
 @app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route("/opencam", methods=['GET'])
 def opencam():
@@ -33,6 +34,17 @@ def stopterminal():
     else:
         print("No process running")
     return "OK"
+
+@app.route("/image_feed")
+def image_feed():
+    # Path lengkap ke gambar
+    image_path = os.path.join(IMAGE_FOLDER, "0_detected.jpg")
+
+    # Memeriksa apakah file gambar ada
+    if os.path.exists(image_path):
+        return send_file(image_path, mimetype='image/jpeg')
+    else:
+        return "Image not found", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
